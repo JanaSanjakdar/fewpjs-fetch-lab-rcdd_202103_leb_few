7f0b08136625bbf9ddb8bfea9e2270cb5c6e6b25
jana sanjakdar <jana.sanjakdar@hotmail.com>
2021-04-21 21:22:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,13 +2,13 @@ function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
 fetch('https://anapioficeandfire.com/api/books')
   .then(resp => resp.json())
- .then(json => renderBooks(data));
+ .then(json => renderBooks(json));
 
 }
 
-function renderBooks(data) {
+function renderBooks(json) {
   const main = document.querySelector('main');
-  books.forEach(book => {
+  json.forEach(book => {
     const h2 = document.createElement('h2');
     h2.innerHTML = book.name;
     main.appendChild(h2);
